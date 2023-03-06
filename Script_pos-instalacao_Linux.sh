c2560f084cc8f82e9e314779db82501571e5fe25
Matheus Marcelo <matheusmpmenezes@protonmail.com>
2023-03-06 19:41:19 -0300

    Update Script_pos-instalacao_Linux.sh

--- Script_pos-instalacao_Linux.sh
+++ Script_pos-instalacao_Linux.sh
@@ -1,95 +1,54 @@
-#Salvar o arquivo em formato .sh, e executar com previlégios de adm.
+#!/bin/bash
 
-#!/usr/bin/env bash
-# ----------------------------- VARIÁVEIS ----------------------------- #
+# Atualiza a lista de pacotes
+sudo apt-get update
 
-URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
-URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
+# Instalação do pacote snap
+sudo apt-get install snapd
 
-DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+# Instalação do navegador Brave
+sudo snap install brave
 
-PROGRAMAS_PARA_INSTALAR=(
-  snapd
-  darktable
-  ubuntu-restricted-extras
-  virtualbox
-  flameshot
-  steam-installer
-  steam-devices
-  steam:i386
-  brave
-  discord
-  code
-  telegram-desktop
-  wine
-  kate
-  libreoffice-l10n-pt-br
-)
+# Instalação do LibreOffice
+sudo apt-get install libreoffice
 
-# ---------------------------------------------------------------------- #
+# Instalação do Visual Studio Code
+sudo snap install --classic code
 
-# ----------------------------- REQUISITOS ----------------------------- #
+# Instalação do Anki
+sudo apt-get install anki
 
-## Removendo travas eventuais do apt ##
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
+# Instalação do calc
+sudo apt-get install gnome-calculator
 
-## Adicionando/Confirmando arquitetura de 32 bits ##
-sudo dpkg --add-architecture i386
+# Instalação do Flameshot
+sudo apt-get install flameshot
 
-## Atualizando o repositório ##
-sudo apt update -y
+# Instalação do Kate
+sudo apt-get install kate
 
-## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech)##
-sudo apt-add-repository universe -y
-sudo add-apt-repository ppa:obsproject/obs-studio -y
-wget -nc "$URL_WINE_KEY"
-sudo apt-key add winehq.key
-sudo apt-add-repository "deb $URL_PPA_WINE focal main"
-# ---------------------------------------------------------------------- #
+# Instalação do PyCharm Community Edition
+sudo snap install pycharm-community --classic
 
-# ----------------------------- EXECUÇÃO ----------------------------- #
+# Instalação do Steam
+sudo apt-get install steam
 
-## Atualizando o repositório depois da adição de novos repositórios ##
-sudo apt update -y
+# Instalação do VirtualBox
+sudo apt-get install virtualbox
 
-## Download e instalação de programas externos ##
-mkdir "$DIRETORIO_DOWNLOADS"
+# Instalação do Wine
+sudo apt-get install wine
 
-## Instalando pacotes .deb baixados na sessão anterior ##
-sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+# Instalação do VLC
+sudo apt-get install vlc
 
-## Corrigindo pequenos erros de instalação
-sudo apt --fix-broken install -y
+# Instalação do Discord
+sudo snap install discord
 
-# Instalando programas no apt
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    apt install "$nome_do_programa" -y
-  else
-    echo "[INSTALADO] - $nome_do_programa"
-  fi
-done
+echo "Instalação concluída!"
 
-# Instalando Wine
-sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
 
-## Remover programas ##
-sudo apt remove gimp -y
 
-## Instalando suporte Flatpak ##
-sudo apt install gnome-software-plugin-flatpak
-
-## Instalando pacotes Snap ##
-sudo snap install spotify
-
-# ---------------------------------------------------------------------- #
-
-# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-
-## Finalização, atualização e limpeza##
-sudo apt update && sudo apt dist-upgrade -y
-sudo apt autoclean
-sudo apt autoremove -y
-
-# ---------------------------------------------------------------------- #
+##alvar o script com a extensão .sh e executar com permissões de superusuário. 
+#Abra o terminal na pasta onde o script foi salvo e execute o comando:
+#sudo bash nome-do-script.sh
